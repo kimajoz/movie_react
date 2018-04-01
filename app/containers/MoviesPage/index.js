91e3b6763d10a5cdbf4eda1c857d1dcc4f954c7a
billettepy <billettepy@MacBook-Air-de-Matthias.local>
2018-04-01 12:09:21 +0200

    before no-useless-concat and arrow callback

--- app/containers/MoviesPage/index.js
+++ app/containers/MoviesPage/index.js
@@ -13,93 +13,41 @@ import List from './List';
 import ListItem from './ListItem';
 import ListItemTitle from './ListItemTitle';
 
-export default class MoviesPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
-
-  // Since state and props are static,
-  // there's no need to re-render this component
-  shouldComponentUpdate() {
-    return false;
-  }
+export default class MoviesPage extends React.Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      movies: []
+      movies: [],
     };
-  }  
-  
-  fetchMovieId(id) {
-    var that = this;
-   
-    function logArrayElements(element, index, array) {
-      //console.log('i[' + index + '] = ' + element);
-      //element.forEach(logArrayElements);
-	Object.keys(element).forEach(function(key) {
-  		//var value = element[key];
-  		//console.log(element[key]);
-		//movie += "<li>{element['id']}</li>";
-		var movie = [];
-		movie += '<li>';
-		//movie += [{element['title']}];
-		movie += '</li>';
-  		console.log('id:' + element['id']);
-  		console.log('title:' + element['title']);
-  		console.log('movie:' + movie);
-	});
-	document.getElementById('res').innerHTML = movie;
-    }
-
-   if (id) {
-      fetch('http://localhost:3000/api/movies:' + id).then(function (response) {
-        return response.json();
-      }).then(function (result) {
-        console.log(result);
-        that.setState({ movies: result });
-        console.log(that.state.movies);
-	//console.log(result.forEach(logArrayElements));
-      });
-    }
   }
-
-  fetchFirst(url) {
-    var that = this;
-
-    var movie = [];
-    function printMovieId(element, index, array) {
-      //console.log('i[' + index + '] = ' + element);
-        movie[index] = '<ul>';
-	movie[index] += '<div><h2 id=id_' + element['id'] + '>' + element['title'] + '</h2> by <a href=\'#\' class=director>' + element['director'] + '</a></div>';
-	movie[index] += '<img src=' + element['image'] + '/>';
-	movie[index] += '<div><a href=' + element['movielink'] + '>' + element['movielink'] + '</a></div>';
-	movie[index] += '<p id=desc>' + element['desc'] + '</p>';
-	Object.keys(element).forEach(function(key) {
-  		//var value = element[key];
-  		//console.log(element[key]);
-		movie[index] += '<li>' + element[key] + '</li>';
-		//movie[index] += '<li>' + element['id'] + '</li>';
-		//movie[index] += '<li>' + element['title'] + '</li>';
-  		//console.log('id:' + element['id']);
-  		//console.log('title:' + element['title']);
-	});
-	movie[index] += '</ul>';
-  	console.log('movie:' + movie);
-	document.getElementById('res').innerHTML = movie;
-    }
-    
-    if (url) {
-      fetch('http://localhost:3000/api/movies').then(function (response) {
-        return response.json();
-      }).then(function (result) {
-	result.forEach(printMovieId);
-      });
-    }
-  }  
   componentDidMount() {
-      this.fetchFirst("reactjs");
-      //this.fetchMovieId(1);
+    this.fetchFirst('firstload');
   }
+  shouldComponentUpdate() { // To allow change states
+    return true;
+  }
+  fetchFirst(url) {
+    const movie = [];
+    function printMovieId(element, index) {
+      movie[index] = '<ul>';
+      movie[index] += '<div><h2 id=id_' + element.id + '>' + element.title + '</h2> by <a href=\'#\' class=director>' + element.director + '</a></div>';
+      movie[index] += '<a href=# id=' + element.id + '><img src=' + element.image + '/>';
+      movie[index] += '<div><a href=' + element.movielink + '>' + element.movielink + '</a></div>';
+      movie[index] += '<p id=desc>' + element.desc + '</p>';
+      movie[index] += '</ul>';
+      document.getElementById('res').innerHTML = movie;
+    }
 
+    if (url) {
+      fetch('http://localhost:3000/api/movies').then(function getresp(response) {
+        return response.json();
+      }).then(function forres(result) {
+        result.forEach(printMovieId);
+      });
+    }
+  }
   render() {
     return (
       <div>
@@ -120,14 +68,8 @@ export default class MoviesPage extends React.Component { // eslint-disable-line
             </p>
           </ListItem>
         </List>
-	<div id="res">
-	  <ul>
-            {this.state.movies.map(movies =>
-              <li key={movies.id}>{movies.title}</li>
-			//<img src={movies.movielink} />
-            )}
-          </ul>
-	</div>
+        <div id="res">
+        </div>
       </div>
     );
   }
